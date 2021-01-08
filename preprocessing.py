86060a45a615a17cdcedafdc37f23096fd2bd023
WanBala <37066755+WanBala@users.noreply.github.com>
2021-01-08 19:32:17 +0800

    fix for one hot

--- preprocessing.py
+++ preprocessing.py
@@ -22,7 +22,7 @@ def count_features(df:pd.DataFrame):
         c = df[column].count()
         d[column] = c
     for key in d:
-        print("{0} has {1} entities".format(key, d[key]))
+        print("{} \thas \t{} \tentities".format(key, d[key]))
 
 
 def print_shape_change(f):
@@ -65,7 +65,7 @@ def drop_feature(df:pd.DataFrame):
 
 
 @print_nan_count
-def fill_na(df:pd.DataFrame, number=0):
+def fill_na(df:pd.DataFrame, number:int =0):
     '''
         Replace NaN with a specified number for the columns listed in the feature_fillna.
     '''
@@ -88,20 +88,6 @@ def drop_useless_entry(df:pd.DataFrame):
     return this_df
 
 
-def fill_na(df:pd.DataFrame, number:int = 0):
-    '''
-        This function replace the NaN with the speicified number for the columns listed in the feature_fillna.
-    '''
-    return df[feature_fillna].fillna(number)
-    
-
-def drop_feature(df:pd.DataFrame):
-    '''
-        This function removes the columns listed in the feature_del.
-    '''
-    return df.drop(feature_del)
-
-
 @print_shape_change
 def ont_hot(df:pd.DataFrame):
     '''
@@ -120,6 +106,28 @@ def one_hot(df:pd.DataFrame):
     return pd.get_dummies(df[feature_one_hot])
 
 
+def one_hot_encoding(df1:pd.DataFrame, df2:pd.DataFrame):
+    '''
+        One-hot encoding for the features listed in *feature_one_hot*
+    '''
+    features_df1 = df1[feature_one_hot]
+    features_df2 = df2[feature_one_hot]
+
+    num = features_df1.shape[0]
+    one_hot_pd = pd.concat([features_df1, features_df2])
+    one_hot_pd = one_hot(one_hot_pd)
+
+    one_hot_df1 = one_hot_pd.iloc[:num]
+    one_hot_df2 = one_hot_pd.iloc[num:]
+
+    df1 = df1.drop(feature_one_hot, axis=1)
+    df2 = df2.drop(feature_one_hot, axis=1)
+
+    df1 = pd.concat([df1, one_hot_df1], axis=1)
+    df2 = pd.concat([df2, one_hot_df2], axis=1)
+    return df1, df2
+
+
 ############################################################################################################################################
 #                                                           Main
 ############################################################################################################################################
@@ -128,7 +136,6 @@ def one_hot(df:pd.DataFrame):
 feature_predict_cancel = ['is_repeated_guest', 'previous_cancellations', 'previous_bookings_not_canceled', 'deposit_type']
 feature_del            = ['company']
 feature_fillna         = ['agent']
-
 feature_one_hot        = ['hotel', 'arrival_date_month', 'meal', 'country', 'market_segment', 'distribution_channel', 'reserved_room_type',
                         'assigned_room_type', 'deposit_type', 'agent', 'customer_type', 'day_of_the_week']
 
@@ -146,8 +153,7 @@ if __name__ == '__main__':
     train_df = drop_useless_entry(train_df)
     test_df  = drop_useless_entry(test_df)
 
-    train_df = ont_hot(train_df)
-    test_df  = ont_hot(test_df)
+    train_df, test_df = one_hot_encoding(train_df, test_df)
 
     train_df.to_csv('train_df_out.csv', index=False)
     test_df.to_csv('test_df_out.csv', index=False)
