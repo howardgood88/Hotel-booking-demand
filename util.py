2cd09214adb3748aa8d6ef25831aebe526c3f8b8
WanBala <37066755+WanBala@users.noreply.github.com>
2021-01-09 18:01:27 +0800

    update

--- util.py
+++ util.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
 
-DATA_TRAIN = "Dataset\\train_final.csv"
-DATA_TEST  = "Dataset\\test_final.csv"
+DATA_TRAIN   = "Dataset\\train_final.csv"
+DATA_TRAIN_Y = "Dataset\\train_label.csv"
+DATA_TEST    = "Dataset\\test_final.csv"
 
-feature_drop = ['ID']
+feature_train_drop = ['ID', 'reservation_status', 'reservation_status_date', 'concat_date']
+feature_test_drop  = ['ID', 'concat_date']
+
 
 def read_file(path):
     return pd.read_csv(path)
 
 
 def get_data():
-    return read_file(DATA_TRAIN), read_file(DATA_TEST)
+    return read_file(DATA_TRAIN).drop(feature_train_drop), read_file(DATA_TEST).drop(feature_test_drop)
 
 
 
