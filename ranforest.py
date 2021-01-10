3c3c628817b9bebf39a7eb1756b02b1d9372772b
WanBala <37066755+WanBala@users.noreply.github.com>
2021-01-10 17:54:30 +0800

    take is_canceled into considering

--- ranforest.py
+++ ranforest.py
@@ -50,7 +50,7 @@ if __name__ == '__main__' and MODE:
         print('Model loaded success.')
         print('Accuracy is_canceled:', clf.score(X, is_canceled))
     else:
-        clf = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=10, verbose=True), verbose=True)
+        clf = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=100, verbose=True), verbose=True)
         clf.fit(X, is_canceled)
         print('is_canceled training finished...')
         print('Accuracy is_canceled:', clf.score(X, is_canceled))
@@ -65,7 +65,7 @@ if __name__ == '__main__' and MODE:
         print('Model loaded success.')
         print('Accuracy adr:', clf2.score(X, adr))
     else:
-        clf2 = make_pipeline(MinMaxScaler(), RandomForestRegressor(n_estimators=10, verbose=True), verbose=True)
+        clf2 = make_pipeline(MinMaxScaler(), RandomForestRegressor(n_estimators=100, verbose=True), verbose=True)
         clf2.fit(X, adr)
         print('adr training finished...')
         print('accuracy adr:', clf2.score(X, adr))
@@ -97,7 +97,7 @@ if __name__ == '__main__' and MODE:
         print('Model loaded success.')
         print('Accuracy scale:', clf3.score(daily_revenue_list[:, np.newaxis], scale_label))
     else:
-        clf3 = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=10, verbose=True), verbose=True)
+        clf3 = make_pipeline(MinMaxScaler(), RandomForestClassifier(n_estimators=100, verbose=True), verbose=True)
         clf3.fit(daily_revenue_list[:, np.newaxis], scale_label)
         print('scale training finished...')
         print('accuracy scale:', clf3.score(daily_revenue_list[:, np.newaxis], scale_label))
@@ -115,11 +115,15 @@ if __name__ == '__main__' and MODE != 1:
     test_x = test_x.drop(drop_features, axis=1)
     is_canceled = clf1.predict(test_x)
 
+    is_canceled_drop = [i for i, x in enumerate(is_canceled) if x == 1]
+    test_x = test_x.drop(is_canceled_drop, axis=0)
+
     adr = clf2.predict(test_x)
-    
+
     stay_nights = test_x['stays_in_weekend_nights'] + test_x['stays_in_week_nights']
     request_revenue = adr * stay_nights
     days = test_x.filter(regex=('arrival_date_day_of_month_*'))
+
     daily_revenue = 0
     daily_revenue_list = []
     for idx in range(len(days)):
@@ -127,7 +131,7 @@ if __name__ == '__main__' and MODE != 1:
             daily_revenue_list.append(daily_revenue)
             daily_revenue = 0
         else:
-            daily_revenue += request_revenue[idx]
+            daily_revenue += request_revenue.iloc[idx]
     daily_revenue_list.append(daily_revenue)
     daily_revenue_list = np.array(daily_revenue_list)
     assert(len(daily_revenue_list) == len(test_y))
